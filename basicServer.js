cd5e7bc2be3bc8f70766d570297dc25ab4824e2f
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-16 15:00:53 +0200

    basic node.js and mongodb

---
+++ basicServer.js
@@ -0,0 +1,45 @@
+const { createServer } = require("http");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+//Set up a connection to MongoDB
+
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const server = createServer((req, res) => {
+  // connect to MongoDB
+  run().catch(console.dir);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    const myDB = client.db("myDB");
+    const myColl = myDB.collection("pizzaMenu");
+    const doc = { name: "Neapolitan pizza", shape: "round" };
+    const result = await myColl.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    console.log("result object", result);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
